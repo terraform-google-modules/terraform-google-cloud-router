57a612f49663902f122dd8460c919be0a4938b84
Rama Balla <ramakrishna.balla@gmail.com>
2021-12-21 10:55:02 +1100

    Update main.tf

--- examples/nat/main.tf
+++ examples/nat/main.tf
@@ -25,6 +25,16 @@ module "cloud_router" {
 
   nats = [{
     name = "my-nat-gateway"
+    enable_endpoint_independent_mapping = true,
+    high_availability                   = true,
+    icmp_idle_timeout_sec               = 30,
+    min_ports_per_vm                    = var.min_ports_per_vm,
+    max_ports_per_vm                    = var.max_ports_per_vm,
+    nat_ip_allocate_option              = "AUTO_ONLY",
+    source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES",
+    tcp_established_idle_timeout_sec    = 1200,
+    tcp_transitory_idle_timeout_sec     = 30,
+    udp_idle_timeout_sec                = 30,
   }]
 }
 # [END cloudnat_simple_create]

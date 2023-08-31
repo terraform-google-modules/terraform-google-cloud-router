7a4b996223ac36cbf85e33da78f23b985dfd9de8
Pierre Guinoiseau <pierre@guinoiseau.nz>
2023-08-31 22:51:57 +1200

    chore: add a subnet in the NAT example

--- examples/nat/main.tf
+++ examples/nat/main.tf
@@ -21,7 +21,21 @@ module "vpc" {
   project_id   = var.project_id
   network_name = "test-network"
   routing_mode = "GLOBAL"
-  subnets      = []
+  subnets = [
+    {
+      subnet_name   = "test-subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-central1"
+    }
+  ]
+  secondary_ranges = {
+    test-subnet-01 = [
+      {
+        range_name    = "test-subnet-01-secondary-01"
+        ip_cidr_range = "192.168.64.0/24"
+      },
+    ]
+  }
 }
 
 
@@ -35,7 +49,15 @@ module "cloud_router" {
   region  = "us-central1"
 
   nats = [{
-    name = "my-nat-gateway"
+    name                               = "my-nat-gateway"
+    source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+    subnetworks = [
+      {
+        name                     = module.vpc.subnets["us-central1/test-subnet-01"].id
+        source_ip_ranges_to_nat  = ["PRIMARY_IP_RANGE", "LIST_OF_SECONDARY_IP_RANGES"]
+        secondary_ip_range_names = module.vpc.subnets["us-central1/test-subnet-01"].secondary_ip_range[*].range_name
+      }
+    ]
   }]
 }
 # [END cloudnat_simple_create]

8354a5ffc2b6cb5c26426a0bdf05b4b09b6664a1
NandiniAgrawal15 <agrawalnandini@google.com>
2025-12-10 05:23:41 +0000

    Add 'stack_type' variable for IPv6 Interconnect Attachment

--- examples/interconnect_attachment/main.tf
+++ examples/interconnect_attachment/main.tf
@@ -15,12 +15,12 @@
  */
 
 module "cloud_router" {
-  source = "../.."
+  source = "NandiniAgrawal15/cloud-router/google"
 
-  name    = "example-router"
+  name       = "example-router"
   project_id = "example-project"
-  network = "default"
-  region  = "us-central1"
+  network    = "default"
+  region     = "us-central1"
 
   bgp = {
     asn               = 65000
@@ -29,8 +29,8 @@ module "cloud_router" {
 }
 
 module "interconnect_attachment" {
-  source  = "terraform-google-modules/cloud-router/google//modules/interconnect_attachment"
-  version = "~> 7.0"
+  source = "NandiniAgrawal15/cloud-router/google//modules/interconnect_attachment"
+  # version = "~> 7.0"
 
   name    = "example-attachment"
   project = "example-project"

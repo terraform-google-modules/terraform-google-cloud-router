c7985e9f4b9bbfc7a01f5c45fefc008fd27556ff
yuktatechgog <yuktatech@google.com>
2025-12-18 20:22:46 +0000

    feat: Address PR feedback
    - Remove null_resource validation, use variable descriptions instead
    - Revert copyright year change

--- modules/interface/versions.tf
+++ modules/interface/versions.tf
@@ -22,10 +22,6 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.12, < 8"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
   }
 
   provider_meta "google" {
